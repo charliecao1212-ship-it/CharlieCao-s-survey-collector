673b9bdea1aa9c37d6757fb5487806ef4e8db96c
charliecao1212-ship-it <charliecao1212@gmail.com>
2026-01-02 20:16:35 +0800

    last modified edition

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/CharlieCao-s_survey-attempt-collector/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    historyApiFallback: true
+}})
